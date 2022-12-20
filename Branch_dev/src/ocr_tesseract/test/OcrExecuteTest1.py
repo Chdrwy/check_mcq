66b7d1c36e122b64809831296f79bb6229ff25dd
andmarruda <andmarruda@gmail.com>
2022-12-20 20:49:41 -0300

    Adding download image class to test OCR implementation.

--- Branch_dev/src/ocr_tesseract/test/OcrExecuteTest1.py
+++ Branch_dev/src/ocr_tesseract/test/OcrExecuteTest1.py
@@ -1,5 +1,12 @@
 from src.OcrTesseract import OcrTesseract
+from src.DownloadHttpImage import DownloadHttpImage
 import os
 
-ot = OcrTesseract(os.getcwd()+'/../utils/image.png')
+#downloading some image to test ocr using tesseract
+di = DownloadHttpImage('https://jeroen.github.io/images/testocr.png')
+filename = di.download(os.getcwd())
+
+#starting instanceof OcrTesseract that will use tesseract to convert image to character
+#using optical character recognition - OCR
+ot = OcrTesseract(os.getcwd()+'/'+filename)
 text = ot.execute()
