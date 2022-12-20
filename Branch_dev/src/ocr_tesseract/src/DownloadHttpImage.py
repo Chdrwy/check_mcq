507d37550cd9055ab5567e99c62b7d43ca0c4539
andmarruda <andmarruda@gmail.com>
2022-12-20 20:49:19 -0300

    Improve image download system and example, now get filename automatically

--- Branch_dev/src/ocr_tesseract/src/DownloadHttpImage.py
+++ Branch_dev/src/ocr_tesseract/src/DownloadHttpImage.py
@@ -1,9 +1,12 @@
 import requests
-
+import re
 class DownloadHttpImage:
     def __init__(self, url):
+        self._filename = re.search('\/(?=[^\/]*$).*', url).group(0).lstrip('/')
         self._url = url
 
     def download(self, path):
-        with open(path, 'wb') as f:
+        with open(path + '/' + self._filename, 'wb') as f:
             f.write(requests.get(self._url).content)
+
+        return self._filename
--- Branch_dev/src/ocr_tesseract/utils/download.py
+++ Branch_dev/src/ocr_tesseract/utils/download.py
@@ -2,4 +2,4 @@ from src.DownloadHttpImage import DownloadHttpImage
 import os
 
 di = DownloadHttpImage('https://jeroen.github.io/images/testocr.png')
-di.download(os.getcwd()+'/image.png')
+di.download(os.getcwd())
